b881584e823e9a75113eb59b36ed535b129d462e
IvyChooNie <72536620+IvyChooNie@users.noreply.github.com>
2021-01-17 01:13:00 +0800

    Add files via upload

---
+++ montageToMainPart.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class montageToMainPart : MonoBehaviour
+{
+    public void load()
+    {
+        switch (this.gameObject.name)
+        {
+            case "storyButton":
+                SceneManager.LoadScene("1");
+                break;
+
+
+
+            case "readButton":
+                SceneManager.LoadScene("Menu");
+                break;
+
+            case "phonicButton":
+                SceneManager.LoadScene("firstScene");
+                break;
+        }
+    }
+    }
