b2c1c147424e551c63f8c7f823f9ae7412821e80
IvyChooNie <72536620+IvyChooNie@users.noreply.github.com>
2021-01-17 09:09:23 +0800

    Add files via upload

---
+++ backToMainMon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class backToMainMon : MonoBehaviour
+{
+    public void load()
+    {
+        switch (this.gameObject.name)
+        {
+            case "monHomeButton":
+                SceneManager.LoadScene("DemoScene");
+                break;
+
+            case "monHomeButton01":
+                SceneManager.LoadScene("DemoScene");
+                break;
+
+            case "monHomeButton02":
+                SceneManager.LoadScene("DemoScene");
+                break;
+        }
+    }
+}
